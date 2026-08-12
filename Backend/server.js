3705d9b5d19cb18e8532d70327575ec0e70ae35a
Rohit <khandagalerohit223@gmail.com>
2026-08-12 22:11:04 +0530

    Fix port for deployment, add start script

--- Backend/server.js
+++ Backend/server.js
@@ -5,8 +5,8 @@ const connectTODB = require("./src/config/database")
 
 connectTODB()
 
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
