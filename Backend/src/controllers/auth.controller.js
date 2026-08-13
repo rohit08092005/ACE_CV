23037eecbd7ae19813a93702037baf18e6a414bc
Rohit <khandagalerohit223@gmail.com>
2026-08-13 08:12:52 +0530

    fixed some issues

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -42,7 +42,12 @@ async function registerUserController(req, res) {
     )
 
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000 // 1 day, matches your JWT expiry
+})
 
     res.status(201).json({
         message: "User registered successfully",
@@ -86,7 +91,12 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+})
     res.status(200).json({
         message: "User logged in successfully",
         user: {
