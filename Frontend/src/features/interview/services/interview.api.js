571e80bd0bbd1a7495a02cd007677136023a247d
Rohit <khandagalerohit223@gmail.com>
2026-08-13 11:46:12 +0530

    Fix interview.api.issues

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -16,11 +16,13 @@ export const generateInterviewReport =async({jobDescription, selfDescription, re
     formData.append("selfDescription",selfDescription)
     formData.append("resume",resumeFile)
 
-    const response= await api.post("/api/interview/" , formData,{
-        headers:{
-            "Content-Type": "multipart/form-data"
-        }
-    })
+    // const response= await api.post("/api/interview/" , formData,{
+    //     headers:{
+    //         "Content-Type": "multipart/form-data"
+    //     }
+    // })
+
+    const response = await api.post("/api/interview/", formData)
 
     return response.data
 }
